7d44ff30699e5b98d67ab9257b8c605e58c876ac
Sai Kalyan <kalyan9700128@gmail.com>
2023-06-01 20:43:57 +0530

    Firsttime Commit

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def indexpage():
     return render_template('Index.html')
 
 
-@app.route('/Collaborate')
-def collaborate():
-    return render_template('Collaborate.html')
-
-
 @app.route('/Services')
 def services():
     return render_template('Services.html')
+
+
+@app.route('/Collaborate')
+def collaborate():
+    return render_template('Collaborate.html')
 
 
 if __name__ == '__main__':
